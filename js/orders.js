fd782b9d8ecc4e3ab80a16f21679478c384d4e76
kensasaki12 <kensasakixx@gmail.com>
2025-05-08 09:24:22 +0700

    Update orders.js

--- js/orders.js
+++ js/orders.js
@@ -1,17 +1,14 @@
-// orders.js
 $(document).ready(function() {
-    // Khởi tạo DataTable
     $('#ordersTable').DataTable({
         responsive: true,
         language: {
             url: '//cdn.datatables.net/plug-ins/1.10.25/i18n/Vietnamese.json'
         },
         columnDefs: [
-            { orderable: false, targets: [5] } // Tắt sắp xếp cho cột hành động
+            { orderable: false, targets: [5] } 
         ]
     });
 
-    // Xử lý xem chi tiết đơn hàng
     $('.view-order-btn').click(function() {
         const orderId = $(this).data('order-id');
         window.location.href = `order-detail.html?id=${orderId}`;
